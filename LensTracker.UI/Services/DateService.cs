8422dc763b8d229dac9d1ffc3dad52845f3462c2
solingoran99 <solin.goran.yusuf.rasid@iths.se>
2025-12-06 02:31:27 +0100

    css changes

---
+++ LensTracker.UI/Services/DateService.cs
@@ -0,0 +1,17 @@
+﻿namespace LensTracker.UI.Services
+{
+    public class DateService
+    {
+        public int SelectedDay { get; private set; }
+        public string FormattedDate { get; private set; } = "";
+        public event Action? OnChange;
+
+        public void SetDay(int day)
+        {
+            SelectedDay = day;
+            string month = DateTime.Now.ToString("MMMM");
+            FormattedDate = $"{day} {month}";
+            OnChange?.Invoke();
+        }
+    }
+}
